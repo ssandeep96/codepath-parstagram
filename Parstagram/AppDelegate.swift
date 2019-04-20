dc02ddd9b3d0fd1f7a764c4527e82316559cda19
Sandeep Sagoo <ssagoo@ucsd.edu>
2019-04-19 17:08:25 -0700

    error files

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -24,8 +24,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             })
         )
         
+        if PFUser.current() != nil{
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            
+            window?.rootViewController = feedNavigationController
+            
+        }
+        
         return true
     }
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
